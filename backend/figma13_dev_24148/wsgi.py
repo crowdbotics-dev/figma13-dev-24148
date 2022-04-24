ec38e5a0b8a4815b6867852eea04780a74c6498d
Crowdbotics <engineering@crowdbotics.com>
2022-04-24 09:26:49 +0000

    Initial commit.

---
+++ backend/figma13_dev_24148/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for figma13_dev_24148 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'figma13_dev_24148.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "figma13_dev_24148",
+  url: "https://figma13_dev_24148.botics.co",
+  api: "https://figma13_dev_24148.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
